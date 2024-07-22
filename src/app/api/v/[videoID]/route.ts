c560ef88e551c54c19ca9dd1a9e41a2c08903bf5
oasobi <oasobyy@gmail.com>
2024-07-22 10:34:29 +0000

    update

--- src/app/api/v/[videoID]/route.ts
+++ src/app/api/v/[videoID]/route.ts
@@ -4,7 +4,7 @@ export async function GET(req: Request, {params}: { params: {videoID: string}}) 
     const vid = params.videoID;
     const apiURL = "https://invidious.jing.rocks/api/v1/videos/";
 
-    const res = await fetch(apiURL + vid + "?hl=ja");
+    const res = await fetch(apiURL + vid + "?hl=ja&region=jp");
     const data = await res.json();
     return NextResponse.json(data)
 }
