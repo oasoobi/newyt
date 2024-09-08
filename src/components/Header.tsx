51c161d30bee953c371608553f9be2eef98060da
oasobi <oasobyy@gmail.com>
2024-09-08 21:27:12 +0900

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export function Header() {
         ) : (
             <div className="z-50 fixed">
                 <noscript>JavaScriptを有効にしてください。</noscript>
-                <div className="flex items-center md:pl-20 md:pr-20 justify-center md:justify-between w-screen bg-[#ffffffb6] backdrop-blur-lg h-24 pt-12">
+                <div className="flex items-center md:pl-20 md:pr-20 justify-center md:justify-between w-screen bg-[#ffffffb6] backdrop-blur-lg h-24 pt-8">
                     <div className="flex items-center">
                         <div className="pr-2 mr-2 md:mr-0 cursor-pointer">
                             <svg onClick={back} viewBox="0 0 24 24" width="24" height="24" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 8 8 12 12 16"></polyline><line x1="16" y1="12" x2="8" y2="12"></line></svg>
